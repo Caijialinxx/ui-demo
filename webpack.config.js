7f50236a2bf2c67cd9cef9e19ae5147478a90234
caijialinxx <caijialinxx@foxmail.com>
2019-05-06 18:57:11 +0800

    index.tsx -> index.js

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    index: './lib/index.tsx'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/lib'),
+    library: 'CUI',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
+      }
+    ]
+  }
+}
