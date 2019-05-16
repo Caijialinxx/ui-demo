9fe3ebf3fe156b3125b834c493760a82161cdce3
caijialinxx <caijialinxx@foxmail.com>
2019-05-16 18:20:45 +0800

    publish

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,13 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import Icon from './Icon';
 
-const fn = (e) => {
-  console.log(e.target.href);
-};
-
-ReactDOM.render(<div>
-  <Icon name="emoji-happy" className="icon" onClick={fn} onMouseEnter={() => console.log('enter')}/>
-  <Icon name="emoji-cool"/>
-  <Icon name="emoji-shocked"/>
-</div>, document.getElementById('app'));
+export {Icon};
