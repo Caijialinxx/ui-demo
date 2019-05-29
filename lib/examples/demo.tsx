c41afe99f4cd8cdeb9540a89248ef5d5b5636b2d
caijialinxx <caijialinxx@foxmail.com>
2019-05-29 11:34:51 +0800

    代码高亮

--- lib/examples/demo.tsx
+++ lib/examples/demo.tsx
@@ -1,4 +1,6 @@
 import React, {ReactNode, useState} from 'react';
+import Highlight, {defaultProps} from 'prism-react-renderer';
+import codeTheme from 'prism-react-renderer/themes/vsDark.js';
 
 interface DemoProps {
   code: string;
@@ -7,12 +9,26 @@ interface DemoProps {
 
 const Demo: React.FunctionComponent<DemoProps> = (props) => {
   const [showCode, setShowCode] = useState(false);
+  const code = (
+    <Highlight {...defaultProps} theme={codeTheme} code={props.code} language="typescript">
+      {({className, style, tokens, getLineProps, getTokenProps}) => (
+        <pre className={className} style={style}>
+              {tokens.map((line, i) => (
+                <div {...getLineProps({line, key: i})}>
+                  {line.map((token, key) => (
+                    <span {...getTokenProps({token, key})} />
+                  ))}
+                </div>
+              ))}
+            </pre>
+      )}
+    </Highlight>);
   return (
     <div className="demo-wrapper">
       <div className="demo-box">
         {props.component}
         <button onClick={() => setShowCode(!showCode)}>Code</button>
-        {showCode && (<pre>{props.code}</pre>)}
+        {showCode && code}
       </div>
     </div>);
 };
